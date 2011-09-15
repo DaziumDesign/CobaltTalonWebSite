885360273bb90fbe57e9b0496f1e1cdb4339ded1
Jess Brown <jess@brownwebdesign.com>
2011-09-15 16:32:26 -0400

    add dress up payer products suite

--- pages/products.php
+++ pages/products.php
@@ -19,9 +19,9 @@ include '../partials/_head.php';
         <?php include '../partials/_menu.php'; ?>
         <div id="main" role="main">
           <!--Start Left Column-->
-        <?php
-        include '../partials/_products_menu.php';
-        ?>
+<?php
+include '../partials/_products_menu.php';
+?>
 <!--Start of Main Content-->
 <div class="content">
   <img class="main_image" src="/images/products/data.jpg" />
@@ -29,62 +29,65 @@ include '../partials/_head.php';
   <h2><a href="/products/healthcare">Talon Knowledge Engine&#8482; - For Healthcare</a></h2>
   <p>The product that Talon offers is a fully-integrated, best-in-class, and economically efficient analytic platformconsisting of an industry-leading data warehousing appliance and an equivalent level business intelligence application…all delivered as a managed service and provided on a <em>pay-as-you-go</em> basis. Named <a href="/products/healthcare"><strong>Talon Knowledge Engine&#8482; (TKE) - For Healthcare</strong></a>, the solution covers all of the basics and integrates advanced, proprietary predictive analytic models and methodologies that enable companies to solve the most pressing industry-specific business problems. The end result is significant, tangible economic benefit and ultimately a strategic competitive advantage for Talon customers. </p>
   <!--Healthcare Insurance Payer Module-->
-  <div>
-    <h3 id="paymod">TKE For Healthcare Payer</h3>
-    <img src="/images/products/method.jpg" class="method" />
-    <p>
-      Comprehensive analytic solutions deliver business insight to healthcare
-      insurers so they can better understand and predict the cost
-      of care. Additionally, we provide answers on how to efficiently and effectively
-      market insurance products directly to consumers.</p>
-  </div>
+  <div class="healthcare_payer">
+    <div>
+      <h3 id="paymod">TKE For Healthcare Payer</h3>
+      <img src="/images/products/method.jpg" class="method" />
+      <p>
+        Comprehensive analytic solutions deliver business insight to healthcare
+        insurers so they can better understand and predict the cost
+        of care. Additionally, we provide answers on how to efficiently and effectively
+        market insurance products directly to consumers.</p>
+    </div>
     <!--Talon Analytics Suite of Payer Products-->
-      <div class="blockquote">
-      <h4><a href="/products/payer-products">Payer Products Suite</a></h4>
+    <div class="blockquote">
+      <h4><a href="/products/payer-products"><span>Payer Products Suite</span></a></h4>
       <p>Leveraging historical data on patient claims history, patient demographic/psychographic/behavioral characteristics, we employ statistical models to:</p>
       <ul>
-          <li><a href="/products/payer-products#cost">Cost of Care Analytics </a></li>
-          <li><a href="/products/payer-products#admit">Re-admittance Predictor </a></li>
-          <li><a href="/products/payer-products#treatment">Treatment Predictor </a></li>
-          <li><a href="/products/payer-products#risk">Risk Scoring Analytics </a></li>
-          <li><a href="/products/payer-products#marketing">Marketing Analytics </a></li>
+        <li><img src="/images/products/icons1.jpg" /><a href="/products/payer-products#cost">Cost of Care Analytics </a></li>
+        <li><img src="/images/products/icons2.jpg" /><a href="/products/payer-products#admit">Re-admittance Predictor </a></li>
+        <li><img src="/images/products/icons3.jpg" /><a href="/products/payer-products#treatment">Treatment Predictor </a></li>
+        <li><img src="/images/products/icons5.jpg" /><a href="/products/payer-products#risk">Risk Scoring Analytics </a></li>
+        <li><img src="/images/products/icons4.jpg" /><a href="/products/payer-products#marketing">Marketing Analytics </a></li>
       </ul>
-    <!--Healthcare Industry-Specific Benefits-->
-    <div class="acc_class">       
-      <h4 id="indben"><a class="acc_link" href="#">Payer Benefits</a></h4>
-      <p>Now, more than ever, healthcare payers are faced with complex business and regulatoryopportunities that can no longer be solved without effective analytics. The Talon Analytic Product addresses the following types of analytic capabilities: </p>
-      <p><a class="acc_link" href="#">Read More</a></p>
-      <ul class="acc_text">
-        <li>Predicting and managing medical loss ratios</li>
-        <li>Awareness and management of regulatory risk adjustors</li>
-        <li>Identification of health risk without traditional underwriting processes </li>
-        <li>Improve the health outcomes of their customers </li>
-        <li>Reduced cost of care and in turn lower prices for customers</li>
-        <li>Patient centered medical homes and accountable care organizations </li>
-        <li>Proving return on investment for wellness solutions</li>
-        <li>Consumer segmentation </li>
-        <li>Exploration of diversification strategies</li>
-        <li>Administrative cost containment</li>
-        <li>And much, much more…</li>
-      </ul>
-    </div>
-      </div>
-    <!--Healthcare Provider Module-->
-    <div class="acc_class">       
-      <h3 id="provmod">TKE For Healthcare Provider</h3>
-      <p>Comprehensive predictive analytics place decision tools and criteria in the health professionals’ hands at the point of care.</p>
-      <p><a class="acc_link" href="#">Read More</a></p>
-      <div class="acc_text">
-        <h3>Talon Analytics Suite of Provider Products</h3>
-        <p><strong>Coordination of Care Workbench</strong> provides a set of tools that will interconnect suppliers across the spectrum of care and provide insight and compliance. Also, offers access to the metrics, algorithms, and reports necessary to manage patients, cases, and diseases.</p>
-        <p><strong>Population Health Management Assistant</strong> uses techniques of population statistics and analytics to discern effective treatments and inform physicians of upcoming events and episodes of care to improve outcomes for the managed population.</p>
+      <div class="spacer"></div>
+      <!--Healthcare Industry-Specific Benefits-->
+      <div class="acc_class">       
+        <h4 id="indben"><a class="acc_link" href="#"><span>Payer Benefits</span></a></h4>
+        <p>Now, more than ever, healthcare payers are faced with complex business and regulatoryopportunities that can no longer be solved without effective analytics. The Talon Analytic Product addresses the following types of analytic capabilities: </p>
+        <p><a class="acc_link" href="#">Read More</a></p>
+        <ul class="acc_text">
+          <li>Predicting and managing medical loss ratios</li>
+          <li>Awareness and management of regulatory risk adjustors</li>
+          <li>Identification of health risk without traditional underwriting processes </li>
+          <li>Improve the health outcomes of their customers </li>
+          <li>Reduced cost of care and in turn lower prices for customers</li>
+          <li>Patient centered medical homes and accountable care organizations </li>
+          <li>Proving return on investment for wellness solutions</li>
+          <li>Consumer segmentation </li>
+          <li>Exploration of diversification strategies</li>
+          <li>Administrative cost containment</li>
+          <li>And much, much more…</li>
+        </ul>
       </div>
     </div>
+  </div>
+  <!--Healthcare Provider Module-->
+  <div class="acc_class">       
+    <h3 id="provmod">TKE For Healthcare Provider</h3>
+    <p>Comprehensive predictive analytics place decision tools and criteria in the health professionals’ hands at the point of care.</p>
+    <p><a class="acc_link" href="#">Read More</a></p>
+    <div class="acc_text">
+      <h3>Talon Analytics Suite of Provider Products</h3>
+      <p><strong>Coordination of Care Workbench</strong> provides a set of tools that will interconnect suppliers across the spectrum of care and provide insight and compliance. Also, offers access to the metrics, algorithms, and reports necessary to manage patients, cases, and diseases.</p>
+      <p><strong>Population Health Management Assistant</strong> uses techniques of population statistics and analytics to discern effective treatments and inform physicians of upcoming events and episodes of care to improve outcomes for the managed population.</p>
+    </div>
+  </div>
 </div>
-    <!--Start of Right Column-->
-    <div class="right_col">
-      <?php include '../partials/_products_right.php'; ?>
-    </div> 		
+<!--Start of Right Column-->
+<div class="right_col">
+  <?php include '../partials/_products_right.php'; ?>
+</div> 		
   </div>
 </div>
 <!-- eo #container -->
