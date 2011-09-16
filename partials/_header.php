d92ed16728569803ff15e711e71e1d7c4414dac9
Jess Brown <jess@brownwebdesign.com>
2011-09-16 17:01:26 -0400

    initial cobalt changes

--- partials/_header.php
+++ partials/_header.php
@@ -1,4 +1,4 @@
 <header>
-  <div class="logo"><a href="/"><img src="/images/layout/logo.jpg" /></a></div>
+  <div class="logo"><a href="/"><img src="/images/layout/logo.png" /></a></div>
   <div class="calltoaction"><img src="/images/layout/tag.jpg" /></div>
 </header>
