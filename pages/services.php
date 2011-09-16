3f7cf89d6550b356066d406db54faf1c59a736ba
Jess Brown <jess@brownwebdesign.com>
2011-09-16 17:24:47 -0400

    fix widht of acc text when images are present

--- pages/services.php
+++ pages/services.php
@@ -52,43 +52,45 @@
               			<p>Cobalt Talon Analytics is a software company offering solutions designed to solve complex business challenges using advanced analytics. Cobalt Talon specializes in consumer, predictive and cost management analytics. The product that Cobalt Talon offers is a fully-integrated, best-in-class, and economically efficient analytic platform consisting of an industry-leading data warehousing appliance and an equivalent level business intelligence application…all delivered as a managed service and provided on a pay-as-you-go basis.</p>
               			<p>Named <strong>Cobalt Talon Knowledge Engine&#8482; (TKE)</strong>, the solution covers all of the basics and integrates advanced, proprietary predictive analytic models and methodologies that enable companies to solve the most pressing industry-specific business problems. The end result is significant, tangible economic benefit and ultimately a strategic competitive advantage for Cobalt Talon customers. </p>
              		<h3><a href="/services/how-it-works">TKE: How it works</a></h3>
-<!--Recipe-->
-                <h2>Recipe for a Successful Analytic Product</h2>
-                <div class="talon_key">
-                  <img src="/images/services/talon_key.jpg" />
+                <!--Recipe-->
+                <div class="text_with_image">
+                  <h2>Recipe for a Successful Analytic Product</h2>
+                  <div class="talon_key">
+                    <img src="/images/services/talon_key.jpg" />
+                  </div>
+                  <ul class="acc_class">
+                    <li><strong><a class="acc_link" href="#">Top-notch People</a></strong></li>
+                    <ul class="acc_text">
+                      <li>Analytic experts that understand the basics of reporting and analytic solutions as well as cutting-edge, advanced methods such as data mining and statistical model design and deployment</li>
+                      <li>Business Partners that understand how to put all the pieces together in order to leverage technology to drive results</li>
+                      <li>Product experts that serve and listen to the customer and proactively design and develop solutions within the product to return the most value.</li>
+                    </ul>
+                  </ul>
+                  <ul class="acc_class">
+                    <li><strong><a class="acc_link" href="#">World-Class Technologies</a></strong></li>
+                    <ul class="acc_text">
+                      <li><strong><em>Teradata</em></strong> Data Warehousing Appliance (<strong>Gartner Group</strong> Magic Quadrant Leader) - The world's largest company solely focused on creating enterprise agility through database software, enterprise data warehousing, data warehouse appliances, and analytics. It’s all they do. They deliver award-winning, integrated, purpose built platforms based on the most powerful, scalable, and reliable technology platform in the industry. </li>
+                      <li><strong><em>Microstrategy</em></strong> Business Intelligence Software (<strong>Gartner Group</strong> Magic Quadrant Leader) - A global leader in Business Intelligence. Millions of people use their software every day for unlimited data analysis, reports and dashboards, mobile BI, data mining, forecasting, operations management, and executive decision making. </li>
+                      <li>Other technologies required for the solution include - Extract Transform and Load (ETL) tools, Metadata application and repository, data modeling tools, and more</li>
+                    </ul>
+                  </ul>
+                  <ul class="acc_class">
+                    <li><strong><a class="acc_link" href="#">Best-in-Class Analytic Methods</a></strong></li>
+                    <ul class="acc_text">
+                      <li>Targeted advanced analytic methodologies which are relevant to pressing business opportunities (e.g., propensity to buy models) are developed and baked into the product, taking the heavy lifting off of the customer and allowing them to take quick and impactful business action.</li>
+                      <li>Industry specific, commercially available methodologies are also embedded into the solution.&nbsp; These methods are typically used to take raw data into data organized into more meaningful analytic classifications in order to align with industry benchmarks and allow customers to focus on processed information versus raw and often meaningless details.&nbsp; Though a core set of such methods will be included in the base product, customers will have the ability to pick and choose virtually any method that fits their business the best and it can be integrated into the product.&nbsp;&nbsp;&nbsp;&nbsp; </li>
+                    </ul>
+                  </ul>
+                  <ul class="acc_class">
+                    <li><strong><a class="acc_link" href="#">Product Delivery Model</a></strong></li>
+                    <ul class="acc_text">
+                      <li>The product is delivered as a service – Basically, the client provides access to their data, Cobalt Talon processes and enhances the data, Cobalt Talon places the raw and enhanced data into a model that resides on the database platform and accessed via the business intelligence application.</li>
+                      <li>The solution is delivered to the client via a secure, dedicated connection (web-/mobile-based).&nbsp; The client has no new hardware or software to purchase.</li>
+                    </ul>
+                  </ul>
                 </div>
-                	<ul class="acc_class">
-               	  		<li><strong><a class="acc_link" href="#">Top-notch People</a></strong></li>
-                			<ul class="acc_text">
-                  				<li>Analytic experts that understand the basics of reporting and analytic solutions as well as cutting-edge, advanced methods such as data mining and statistical model design and deployment</li>
-                  				<li>Business Partners that understand how to put all the pieces together in order to leverage technology to drive results</li>
-                  				<li>Product experts that serve and listen to the customer and proactively design and develop solutions within the product to return the most value.</li>
-                    		</ul>
-                	</ul>
-                <ul class="acc_class">
-               	  	<li><strong><a class="acc_link" href="#">World-Class Technologies</a></strong></li>
-           		  		<ul class="acc_text">
-           					<li><strong><em>Teradata</em></strong> Data Warehousing Appliance (<strong>Gartner Group</strong> Magic Quadrant Leader) - The world's largest company solely focused on creating enterprise agility through database software, enterprise data warehousing, data warehouse appliances, and analytics. It’s all they do. They deliver award-winning, integrated, purpose built platforms based on the most powerful, scalable, and reliable technology platform in the industry. </li>
-           					<li><strong><em>Microstrategy</em></strong> Business Intelligence Software (<strong>Gartner Group</strong> Magic Quadrant Leader) - A global leader in Business Intelligence. Millions of people use their software every day for unlimited data analysis, reports and dashboards, mobile BI, data mining, forecasting, operations management, and executive decision making. </li>
-           					<li>Other technologies required for the solution include - Extract Transform and Load (ETL) tools, Metadata application and repository, data modeling tools, and more</li>
-                		</ul>
-               		</ul>
-              	<ul class="acc_class">
-                	<li><strong><a class="acc_link" href="#">Best-in-Class Analytic Methods</a></strong></li>
-                		<ul class="acc_text">
-                  			<li>Targeted advanced analytic methodologies which are relevant to pressing business opportunities (e.g., propensity to buy models) are developed and baked into the product, taking the heavy lifting off of the customer and allowing them to take quick and impactful business action.</li>
-                  			<li>Industry specific, commercially available methodologies are also embedded into the solution.&nbsp; These methods are typically used to take raw data into data organized into more meaningful analytic classifications in order to align with industry benchmarks and allow customers to focus on processed information versus raw and often meaningless details.&nbsp; Though a core set of such methods will be included in the base product, customers will have the ability to pick and choose virtually any method that fits their business the best and it can be integrated into the product.&nbsp;&nbsp;&nbsp;&nbsp; </li>
-                		</ul>
-                	</ul>
-           		<ul class="acc_class">
-               		<li><strong><a class="acc_link" href="#">Product Delivery Model</a></strong></li>
-                    	<ul class="acc_text">
-                      		<li>The product is delivered as a service – Basically, the client provides access to their data, Cobalt Talon processes and enhances the data, Cobalt Talon places the raw and enhanced data into a model that resides on the database platform and accessed via the business intelligence application.</li>
-                      		<li>The solution is delivered to the client via a secure, dedicated connection (web-/mobile-based).&nbsp; The client has no new hardware or software to purchase.</li>
-           		  		</ul>
-        		</ul>
-<!--Customer Benefits-->
-              	<div class="customer_benefits_list">
+                <!--Customer Benefits-->
+              	<div class="customer_benefits_list text_with_image">
               		<h2 id="customer_benefits"><a class="acc_link" href="#">Customer Benefits</a></h2>
               			<p>The following lists some of the various benefits that a customer will receive by using the Cobalt Talon Analytic Product.</p>
                     <div class="customer_benefits_micro">
