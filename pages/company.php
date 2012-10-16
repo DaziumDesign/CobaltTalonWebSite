ea103f0f55816d100d90be32e3d40ca02846d9c9
Jess Brown <jess@brownwebdesign.com>
2012-10-16 06:40:12 -0400

    bio update

--- pages/company.php
+++ pages/company.php
@@ -46,7 +46,7 @@
             <h2>Carrie Cowdin</h2>
             <img src="/images/team/carrie-cowdin.jpg" width="100" height="100" alt="Carrie Cowdin" />
             <div class="bio-content">
-              <p>Carrie Cowdin serves as the Senior Director of Product Development for Cobalt Talon. Carrie comes to Cobalt Talon from her consulting firm, Cachet Consulting, which facilitated business analysis and competitive strategy for healthcare companies.</p>
+              <p>Carrie Cowdin serves as the Vice President of Product Development for Cobalt Talon. Carrie comes to Cobalt Talon from her consulting firm, Cachet Consulting, which facilitated business analysis and competitive strategy for healthcare companies.</p>
               <p>Before starting her own business, Carrie attained experience managing several departments at Blue Cross Blue Shield of Kansas City, sustaining roles such as Director of Marketing Systems, Director of the Business Intelligence Competency Center, and Director of the Technology Service Group. Carrie was instrumental in several initiatives while at Blue KC, including the development of award winning programs in the areas of Disease Management and Predictive Modeling. In addition, she helped develop a 360 Member View Dashboard and an Enterprise Data Warehouse for the company. She brings not only many years of experience to Cobalt Talon, but also a variety of expertise.</p>
               <p>Carrie received her Bachelor's degree from Ottawa University and her Master of Business Administration degree from Webster University.</p>
             </div>
