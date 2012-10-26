22ac090d23db616a10a737c12c95293ab785dccf
shadowmud <rsshockley@gmail.com>
2012-10-26 17:37:46 -0400

    services pages

--- pages/services-implementation.php
+++ pages/services-implementation.php
@@ -4,74 +4,73 @@
 <!--[if IE 8 ]>    <html class="no-js ie8" lang="en"> <![endif]-->
 <!--[if (gte IE 9)|!(IE)]><!-->
 <html class="no-js" lang="en">
-  <!--<![endif]-->
-  <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>Cobalt Talon - Knowledge within your grasp</title>
-  <?php
-    include '../partials/_head.php';
-  ?>
-  </head>
-  <body>
-    <div id="root">
-      <div id="wrapper">
-        <div id="container">
-          <?php include '../partials/_header.php'; ?>
-          <?php include '../partials/_menu.php'; ?>
-          <div id="main" role="main">
-          <!--Start Left Column-->
-          <div class="left_col">
-            <?php
-            include '../partials/_services_menu.php';
-            ?>
-          </div>
-            <div class="top_image"></div>
-            <div class="content">
-                   	<div> 	
-                    <h1><strong>Talon Knowledge Engine&#8482; (TKE)</strong></h1>
-                      <h2>Implementation</h2>
-                        <p>The entire implementation process takes place in only 60-90 days, depending on customer data quality and integrity. Our Professional Services team works closely with each of our customers to ensure a successful implementation and get you on the fast track to analysis and to provide timely insight that can make a significant difference in your daily decision-making.</p>
-                    	<p>The Cobalt Talon Analytics implementation includes a methodology and toolbox that allows customers to quickly begin realizing real business value from our services. We focus on the essential data elements that will allow the analytics software to more quickly and more easily provide visibility into your business, both at a macro- and micro-level.</p>
-                    	<p>Your end-users will experience the value of the technology through role-based business intelligence dashboards and&nbsp;Cobalt Talon Analytics templates that help them gain insight, identify issues, and take advantage of market opportunities.</p>
-                    	<p><strong>The implementation is divided into three steps:</strong><br /> </p>
-                      		<ol>
-                           		<li>Data Mapping and Readiness</li>
-                      			<li>Deployment and Data ETL (Extract, Transfer, and Load)</li>
-                  				<li>Testing and Training.</li>
-                        </ol>
-                        <img src="/images/services/steps_graphic.jpg" />
-                        <p><strong>Implementation includes the following components</strong></p>
-              				<ul>
-                  				<li>Cobalt Talon Analytics software functionality</li>
-                  				<li>Cobalt Talon’s TKE functionality</li>
-                  				<li>Automated data loads and verification that the data is successfully and properly loaded</li>
-                  				<li>Testing of the Cobalt Talon Analytics software, including KPIs/metrics, role-specific dashboards, scorecard templates, and mathematical and statistical model verification and validation</li>
-                  				<li>Technical and functional end-user training</li>
-              				</ul>
-                        </div>
-            </div>
-            <div class="right_col">
-              <div class="customer_benefits">
-                <h3><a class="loc" href="#customer_benefits">Customer Benefits</a></h3>
-                	<ul>
-                  		<li>Increased business performance <a href="/services#customer_benefits">More</a></li>
-                  		<li>Strategic competative advantage <a href="/services#customer_benefits">More</a></li>
-                  		<li>Increased efficiency of operations + spending <a href="/services#customer_benefits">More</a></li>
-                  		<li>Pay as you go model <a href="/services#customer_benefits">More</a></li>
-                  		<li>Reduced implementation risk in rolling out <a href="/services#customer_benefits">More</a></li>
-                  		<li>Able to focus on your core business <a href="/services#customer_benefits">More</a></li>
-                	</ul>
-            	</div>
-            </div>
+<!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<title>Cobalt Talon - Knowledge Within Your Grasp</title>
+<?php include '../partials/_head.php'; ?>
+</head>
+<body>
+<div id="root">
+  <div id="wrapper">
+    <div id="container">
+      <?php include '../partials/_header.php'; ?>
+      <?php include '../partials/_menu.php'; ?>
+      <div id="main" role="main">
+        <div class="left_col">
+          <?php include '../partials/_services_menu.php'; ?>
+        </div>
+        <div class="content">
+          <div>
+            <h1>Cobalt Talon Analytics Platform</h1>
+            <h2>Implementation</h2>
+            <p>The entire implementation process takes place in only 60-90 days, depending on customer data quality and integrity. Our Professional Services team works closely with each of our customers to ensure a successful implementation and get you on the fast track to analysis and to provide timely insight that can make a significant difference in your daily decision-making.</p>
+            <p>The Cobalt Talon Analytics implementation includes a methodology and toolbox that allows customers to quickly begin realizing real business value from our services. We focus on the essential data elements that will allow the analytics software to more quickly and more easily provide visibility into your business, both at a macro- and micro-level.</p>
+            <p>Your end-users will experience the value of the technology through role-based business intelligence dashboards and&nbsp;Cobalt Talon Analytics templates that help them gain insight, identify issues, and take advantage of market opportunities.</p>
+            <p><strong>The implementation is divided into three steps:</strong><br />
+            </p>
+            <ol>
+              <li>Data Mapping and Readiness</li>
+              <li>Deployment and Data ETL (Extract, Transfer, and Load)</li>
+              <li>Testing and Training.</li>
+            </ol>
+            <p class="center-text"><img src="/images/services/steps_graphic.jpg" /></p>
+            <p><strong>Implementation includes the following components</strong></p>
+            <ul>
+              <li>Cobalt Talon Analytics software functionality</li>
+              <li>Cobalt Talon Analytics Platform functionality</li>
+              <li>Automated data loads and verification that the data is successfully and properly loaded</li>
+              <li>Testing of the Cobalt Talon Analytics software, including KPIs/metrics, role-specific dashboards, scorecard templates, and mathematical and statistical model verification and validation</li>
+              <li>Technical and functional end-user training</li>
+            </ul>
           </div>
         </div>
-        <!-- eo #container -->
+        <div class="right_col">
+          <div class="customer_benefits">
+            <h3><a class="loc" href="#customer_benefits">Customer Benefits</a></h3>
+            <ul>
+              <li>Increased business performance<br>
+              <span><a href="/services#customer_benefits">More</a></span></li>
+              <li>Strategic competative advantage<br>
+              <span><a href="/services#customer_benefits">More</a></span></li>
+              <li>Increased efficiency of operations + spending<br>
+              <span><a href="/services#customer_benefits">More</a></span></li>
+              <li>Pay as you go model<br>
+              <span><a href="/services#customer_benefits">More</a></span></li>
+              <li>Reduced implementation risk in rolling out<br>
+              <span><a href="/services#customer_benefits">More</a></span></li>
+              <li>Able to focus on your core business<br>
+              <span><a href="/services#customer_benefits">More</a></span></li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <!-- eo #wrapper -->
-      <div id="root_footer"></div>
     </div>
-    <!-- eo #root -->
-    <?php include '../partials/_footer.php'; ?>
-  </body>
+  </div>
+  <div id="root_footer">
+  </div>
+</div>
+<?php include '../partials/_footer.php'; ?>
+</body>
 </html>
