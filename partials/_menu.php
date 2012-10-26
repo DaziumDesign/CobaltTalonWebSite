8158657940bc5e6ad44649fa12616040138de749
shadowmud <rsshockley@gmail.com>
2012-10-26 13:26:17 -0400

    menu

--- partials/_menu.php
+++ partials/_menu.php
@@ -1,7 +1,7 @@
 <nav id="menu-wrap">
   <ul id="menu">
-    <li><a href="#">Home</a></li>
-    <li> <a href="#">Products</a>
+    <li><a href="/">Home</a></li>
+    <li> <a href="/products">Products</a>
       <ul>
         <li><a href="/products/healthcare">Cobalt Talon Analytic Platform</a></li>
         <li><a href="/products/how-it-works ">How it Works</a></li>
@@ -9,27 +9,30 @@
         <li><a href="/products/market-shift-module">MARKET-SHIFT MODULE</a></li>
       </ul>
     </li>
-    <li><a href="#">Services</a></li>
-    <ul>
-      <li><a href="/services/how-it-works">Overview</a></li>
-      <li><a href="/services/implementation">Implementation</a></li>
-      <li><a href="/services#extserv">Extended Services</a></li>
-      <li><a href="/services#training">Training</a></li>
-      <li><a href="/services#techsupport">Tech Support</a></li>
-    </ul>
-    <li><a href="#">Customers</a></li>
-    <ul>
-      <li><a href="/customers#testimonials">Testimonials</a></li>
-      <li><a href="/customers#case">Case Studies</a></li>
-    </ul>
-    <li><a href="#">Company</a></li>
-    <ul>
-      <li><a href="/company#mgt">Leadership</a></li>
-      <li><a href="/company/news">News and Events</a></li>
-      <li><a href="/company/partners">Strategic Alliances</a></li>
-      <li><a href="/company/events">Cobalt Ventures</a></li>
-      <li><a href="/company/careers">Careers</a></li>
-    </ul>
+    <li><a href="/services">Services</a>
+      <ul>
+        <li><a href="/services/how-it-works">Overview</a></li>
+        <li><a href="/services/implementation">Implementation</a></li>
+        <li><a href="/services#extserv">Extended Services</a></li>
+        <li><a href="/services#training">Training</a></li>
+        <li><a href="/services#techsupport">Tech Support</a></li>
+      </ul>
+    </li>
+    <li><a href="/customers">Customers</a>
+      <ul>
+        <li><a href="/customers#testimonials">Testimonials</a></li>
+        <li><a href="/customers#case">Case Studies</a></li>
+      </ul>
+    </li>
+    <li><a href="/company">Company</a>
+      <ul>
+        <li><a href="/company#mgt">Leadership</a></li>
+        <li><a href="/company/news">News and Events</a></li>
+        <li><a href="/company/partners">Strategic Alliances</a></li>
+        <li><a href="/company/events">Cobalt Ventures</a></li>
+        <li><a href="/company/careers">Careers</a></li>
+      </ul>
+    </li>
     <li><a href="#">Contact Us</a></li>
   </ul>
 </nav>
