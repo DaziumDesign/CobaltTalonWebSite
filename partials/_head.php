202794c5b4beb8c14e95cd59b05323c56c2918e2
shadowmud <rsshockley@gmail.com>
2012-10-26 11:40:27 -0400

    drop down menu

--- partials/_head.php
+++ partials/_head.php
@@ -4,7 +4,9 @@
   <link rel="shortcut icon" href="/favicon.ico">
   <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
-  <link rel="stylesheet" href="/css/style.css?v=2">
+  <link rel="stylesheet" href="/css/style.css">
+  <link rel="stylesheet" href="/css/menu.css">
+  
   <script src="/js/libs/modernizr-1.7.min.js"></script>
   <script src="/js/libs/jquery-1.5.1.min.js"></script>
   <script src="/js/mylibs/jquery.cycle.all.min.js"></script>
--- partials/_menu.php
+++ partials/_menu.php
@@ -1,10 +1,34 @@
-<nav>
-<ul class="nav">
-  <li><a href="/">Home</a></li>
-  <li><a href="/products">Products</a></li>
-  <li><a href="/services">Services</a></li>
-  <li><a href="/customers">Customers</a></li>
-  <li><a href="/company">Company</a></li>
-  <li><a class="last" href="/contact">Contact Us</a></li>
+<ul id="menu">
+  <li><a href="#">Home</a></li>
+  <li>
+    <a href="#">Products</a>
+    <ul>
+      <li><a href="/products/healthcare">Cobalt Talon Analytic Platform</a></li>
+      <li><a href="/products/how-it-works ">How it Works</a></li>
+      <li><a href="/products/payer-products">Advanced Analytic Modules</a></li>
+      <li><a href="#">MARKET-SHIFT MODULE</a></li>
+    </ul>
+  </li>
+  <li><a href="#">Services</a></li>
+    <ul>
+      <li><a href="/services/how-it-works">Overview</a></li>
+      <li><a href="/services/implementation">Implementation</a></li>
+      <li><a href="/services#extserv">Extended Services</a></li>
+      <li><a href="/services#training">Training</a></li>
+      <li><a href="/services#techsupport">Tech Support</a></li>
+    </ul>
+  <li><a href="#">Customers</a></li>
+    <ul>
+      <li><a href="/customers#testimonials">Testimonials</a></li>
+      <li><a href="/customers#case">Case Studies</a></li>
+    </ul>
+  <li><a href="#">Company</a></li>
+    <ul>
+      <li><a href="/company#mgt">Leadership</a></li>
+      <li><a href="/company/news">News and Events</a></li>
+      <li><a href="/company/partners">Strategic Alliances</a></li>
+      <li><a href="/company/events">Cobalt Ventures</a></li>
+      <li><a href="/company/careers">Careers</a></li>
+    </ul>
+  <li><a href="#">Contact Us</a></li>
 </ul>
-</nav>
