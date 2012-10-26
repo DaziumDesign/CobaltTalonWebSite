1d1dd6e0c561898bd8d0134a57f5591c2d8cabda
shadowmud <rsshockley@gmail.com>
2012-10-26 17:39:22 -0400

    services pages

--- partials/_footer.php
+++ partials/_footer.php
@@ -53,8 +53,8 @@
 </footer>
 <script>window.jQuery || document.write("<script src='js/libs/jquery-1.5.1.min.js'>\x3C/script>")</script> 
 <!-- scripts concatenated and minified via ant build script--> 
-<script src="js/plugins.js"></script> 
-<script src="js/script.js"></script> 
+<script src="/js/plugins.js"></script> 
+<script src="/js/script.js"></script> 
 <!-- end scripts--> 
 <!--[if lt IE 7 ]>
 <script src="js/libs/dd_belatedpng.js"></script>
