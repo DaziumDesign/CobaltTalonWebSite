950954dec03dd2288ef76867103842f107e43fb5
shadowmud <rsshockley@gmail.com>
2012-10-26 12:48:08 -0400

    menu

--- partials/_menu.php
+++ partials/_menu.php
@@ -1,15 +1,15 @@
-<ul id="menu">
-  <li><a href="#">Home</a></li>
-  <li>
-    <a href="#">Products</a>
-    <ul>
-      <li><a href="/products/healthcare">Cobalt Talon Analytic Platform</a></li>
-      <li><a href="/products/how-it-works ">How it Works</a></li>
-      <li><a href="/products/payer-products">Advanced Analytic Modules</a></li>
-      <li><a href="#">MARKET-SHIFT MODULE</a></li>
-    </ul>
-  </li>
-  <li><a href="#">Services</a></li>
+<nav id="menu-wrap">
+  <ul id="menu">
+    <li><a href="#">Home</a></li>
+    <li> <a href="#">Products</a>
+      <ul>
+        <li><a href="/products/healthcare">Cobalt Talon Analytic Platform</a></li>
+        <li><a href="/products/how-it-works ">How it Works</a></li>
+        <li><a href="/products/payer-products">Advanced Analytic Modules</a></li>
+        <li><a href="/products/market-shift-module">MARKET-SHIFT MODULE</a></li>
+      </ul>
+    </li>
+    <li><a href="#">Services</a></li>
     <ul>
       <li><a href="/services/how-it-works">Overview</a></li>
       <li><a href="/services/implementation">Implementation</a></li>
@@ -17,12 +17,12 @@
       <li><a href="/services#training">Training</a></li>
       <li><a href="/services#techsupport">Tech Support</a></li>
     </ul>
-  <li><a href="#">Customers</a></li>
+    <li><a href="#">Customers</a></li>
     <ul>
       <li><a href="/customers#testimonials">Testimonials</a></li>
       <li><a href="/customers#case">Case Studies</a></li>
     </ul>
-  <li><a href="#">Company</a></li>
+    <li><a href="#">Company</a></li>
     <ul>
       <li><a href="/company#mgt">Leadership</a></li>
       <li><a href="/company/news">News and Events</a></li>
@@ -30,5 +30,6 @@
       <li><a href="/company/events">Cobalt Ventures</a></li>
       <li><a href="/company/careers">Careers</a></li>
     </ul>
-  <li><a href="#">Contact Us</a></li>
-</ul>
+    <li><a href="#">Contact Us</a></li>
+  </ul>
+</nav>
