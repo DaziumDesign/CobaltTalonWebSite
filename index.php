54577a38ec688cf799466f8858197bc336398629
Scott Shockley <scott@brownwebdesign.com>
2012-11-05 04:13:39 -0500

    squared dropdown menu

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<!doctype html>
-<?php include '../partials/_ie.php'; ?>
+<!Doctype html>
+<?php include '/partials/_ie.php'; ?>
 <head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
