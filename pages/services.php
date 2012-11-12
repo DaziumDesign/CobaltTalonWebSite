de142109e954e5c41b96c957d2a7cce9972a7383
Jess Brown <jess@brownwebdesign.com>
2012-11-12 06:49:29 -0500

    menu corrections

--- pages/services.php
+++ pages/services.php
@@ -2,10 +2,34 @@
 <html lang="en">
 <?php include '../partials/_ie.php'; ?>
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>Cobalt Talon - Knowledge Within Your Grasp</title>
   <?php include '../partials/_head.php'; ?>
+  <script src="/js/libs/modernizr-1.7.min.js"></script>
+    <script src="/js/libs/jquery-1.5.1.min.js"></script>
+    <script src="/js/mylibs/jquery.cycle.all.min.js"></script>
+    <script src="/js/mylibs/jquery.scrollTo-min.js"></script>
+    <script src="/js/mylibs/jquery.colorbox-min.js"></script>
+    <script src="/js/main.js"></script>
+    <script type="text/javascript">
+    $(document).ready(function(){
+      var hash = window.location.hash
+      cust_benefits(hash)
+        $('.customer_benefits a').click(function() {
+          cust_benefits(this.hash)
+          return false
+        })
+      function cust_benefits(hash) {
+        var needle=/\#customer_benefits(\d+)/
+        var match = hash.match(needle)
+        if(match) {
+          $.scrollTo('#customer_benefits', 1000);
+          $(hash + ' .acc_text').delay(1200).slideToggle()
+        }
+      }
+    });
+    </script>
 </head>
 <body>
   <div id="root">
@@ -16,76 +40,88 @@
         <div id="main" role="main">
           <div class="left_col">
             <div class="menu local">
-              <?php include '../partials/_services_menu.php'; ?>
-            </div>
+              <?php include '../partials/_services_menu.php'; ?></div>
           </div>
           <div class="content">
             <img class="main_image" src="/images/services/main.jpg" />
             <h2 id="overview">Overview</h2>
-            <p>Now more than ever healthcare payers and providers are faced with complex business opportunities and regulatory challenges that can no longer be solved without effective analytics. Cobalt Ventures and Talon Analytics have partnered to  bring the strength of healthcare management together with healthcare analytics to form Cobalt Talon. Using the Cobalt Talon Analytics Platform for Healthcare, along with Cobalt business modeling, your company will be able to address these new opportunities as well as serve the traditional reporting and analytic needs required in the industry.</p>
+            <p>Now more than ever healthcare payers and providers are faced with complex business opportunities and regulatory challenges that can no longer be solved without effective analytics. Cobalt Ventures and Talon Analytics have partnered to bring the strength of healthcare management together with healthcare analytics to form Cobalt Talon. Using the Cobalt Talon Analytics Platform for Healthcare, along with Cobalt business modeling, your company will be able to address these new opportunities as well as serve the traditional reporting and analytic needs required in the industry.</p>
             <p><strong>Cobalt Talon Analytics Platform</strong> solution covers all of the basics and integrates advanced, proprietary predictive analytic models and methodologies that enable companies to solve the most pressing industry-specific business problems. The end result is significant, tangible economic benefit and ultimately a strategic competitive advantage for Cobalt Talon customers.</p>
-            <!--Recipe-->
-            <div class="text_with_image">
+
+            <div class="customer_benefits_list text_with_image">
               <h2>Recipe for a Successful Analytic Product</h2>
-              <div class="talon_key">
-                <img src="/images/services/talon_key.jpg" />
-              </div>
-              <ul>
+              <div class="talon_key"><img src="/images/services/talon_key.jpg" /></div>
+
+              <ul class="acc_class">
                 <li class="acc_class"><a class="acc_link" href="#">Top-notch People</a>
-                  <ul class="acc_text">
-                    <li>Analytic experts that understand the basics of reporting and analytic solutions as well as cutting-edge, advanced methods such as data mining and statistical model design and deployment</li>
-                    <li>Business Partners that understand how to put all the pieces together in order to leverage technology to drive results</li>
-                    <li>Product experts that serve and listen to the customer and proactively design and develop solutions within the product to return the most value.</li>
-                  </ul>
+                  <div class="acc_text">
+                    <ul style="list-style: none; display: inline">
+                      <li>Analytic experts that understand the basics of reporting and analytic solutions as well as cutting-edge, advanced methods such as data mining and statistical model design and deployment</li>
+                      <li>Business Partners that understand how to put all the pieces together in order to leverage technology to drive results</li>
+                      <li>Product experts that serve and listen to the customer and proactively design and develop solutions within the product to return the most value.</li>
+                    </ul>
+                  </div>
                 </li>
+
                 <li class="acc_class"><a class="acc_link" href="#">World-Class Technologies</a>
-                  <ul class="acc_text">
-                    <li><strong><em>Teradata</em></strong> Data Warehousing Appliance (<strong>Gartner Group</strong> Magic Quadrant Leader) - The world's largest company solely focused on creating enterprise agility through database software, enterprise data warehousing, data warehouse appliances, and analytics. It’s all they do. They deliver award-winning, integrated, purpose built platforms based on the most powerful, scalable, and reliable technology platform in the industry. </li>
-                    <li><strong><em>Microstrategy</em></strong> Business Intelligence Software (<strong>Gartner Group</strong> Magic Quadrant Leader) - A global leader in Business Intelligence. Millions of people use their software every day for unlimited data analysis, reports and dashboards, mobile BI, data mining, forecasting, operations management, and executive decision making. </li>
-                    <li>Other technologies required for the solution include - Extract Transform and Load (ETL) tools, Metadata application and repository, data modeling tools, and more</li>
-                  </ul>
+                  <div class="acc_text">
+                    <ul style="list-style: none; display: inline">
+                      <li><strong><em>Teradata</em></strong> Data Warehousing Appliance (<strong>Gartner Group</strong> Magic Quadrant Leader) - The world's largest company solely focused on creating enterprise agility through database software, enterprise data warehousing, data warehouse appliances, and analytics. It’s all they do. They deliver award-winning, integrated, purpose built platforms based on the most powerful, scalable, and reliable technology platform in the industry.</li>
+                      <li><strong><em>Microstrategy</em></strong> Business Intelligence Software (<strong>Gartner Group</strong> Magic Quadrant Leader) - A global leader in Business Intelligence. Millions of people use their software every day for unlimited data analysis, reports and dashboards, mobile BI, data mining, forecasting, operations management, and executive decision making.</li>
+                      <li>Other technologies required for the solution include - Extract Transform and Load (ETL) tools, Metadata application and repository, data modeling tools, and more</li>
+                    </ul>
+                  </div>
                 </li>
+
                 <li class="acc_class"><a class="acc_link" href="#">Best-in-Class Analytic Methods</a>
-                  <ul class="acc_text">
-                    <li>Targeted advanced analytic methodologies which are relevant to pressing business opportunities (e.g., propensity to buy models) are developed and baked into the product, taking the heavy lifting off of the customer and allowing them to take quick and impactful business action.</li>
-                    <li>Industry specific, commercially available methodologies are also embedded into the solution. These methods are typically used to take raw data into data organized into more meaningful analytic classifications in order to align with industry benchmarks and allow customers to focus on processed information versus raw and often meaningless details. Though a core set of such methods will be included in the base product, customers will have the ability to pick and choose virtually any method that fits their business the best and it can be integrated into the product.</li>
-                  </ul>
+                  <div class="acc_text">
+                    <ul style="list-style: none; display: inline">
+                      <li>Targeted advanced analytic methodologies which are relevant to pressing business opportunities (e.g., propensity to buy models) are developed and baked into the product, taking the heavy lifting off of the customer and allowing them to take quick and impactful business action.</li>
+                      <li>Industry specific, commercially available methodologies are also embedded into the solution.&nbsp; These methods are typically used to take raw data into data organized into more meaningful analytic classifications in order to align with industry benchmarks and allow customers to focus on processed information versus raw and often meaningless details.&nbsp; Though a core set of such methods will be included in the base product, customers will have the ability to pick and choose virtually any method that fits their business the best and it can be integrated into the product.&nbsp;&nbsp;&nbsp;&nbsp;</li>
+                    </ul>
+                  </div>
                 </li>
+
                 <li class="acc_class"><a class="acc_link" href="#">Product Delivery Model</a>
-                  <ul class="acc_text">
-                    <li>The product is delivered as a service – Basically, the client provides access to their data, Cobalt Talon processes and enhances the data, Cobalt Talon places the raw and enhanced data into a model that resides on the database platform and accessed via the business intelligence application.</li>
-                    <li>The solution is delivered to the client via a secure, dedicated connection (web-/mobile-based). The client has no new hardware or software to purchase.</li>
-                  </ul>
+                  <div class="acc_text">
+                    <ul style="list-style: none; display: inline">
+                      <li>The product is delivered as a service – Basically, the client provides access to their data, Cobalt Talon processes and enhances the data, Cobalt Talon places the raw and enhanced data into a model that resides on the database platform and accessed via the business intelligence application.</li>
+                      <li>The solution is delivered to the client via a secure, dedicated connection (web-/mobile-based). The client has no new hardware or software to purchase.</li>
+                    </ul>
+                  </div>
                 </li>
+
               </ul>
             </div>
             <div class="spacer"></div>
+
             <!--Customer Benefits-->
             <div class="customer_benefits_list text_with_image">
               <h2 id="customer_benefits"><a class="acc_link" href="#"></a>Customer Benefits</h2>
+              <div class="customer_benefits_micro"><img src="/images/services/micro.jpg"> Enterprise data with MicroStrategy Mobile.</div>
               <p>The following lists some of the various benefits that a customer will receive by using the Cobalt Talon analytic Product.</p>
-              <div class="customer_benefits_micro">
-                <img src="/images/services/micro.jpg" />
-                Enterprise data with MicroStrategy Mobile.
-              </div>
-              <ul>
+
+              <ul class="acc_class">
                 <li id="customer_benefits1" class="acc_class"><a href="#" class="acc_link">Increased Business Performance</a>
-                  <p class="acc_text">Significantly <strong>improved business performance</strong> measured in terms of significant, tangible, economic benefits, including increased operating/net profit and margins, cash flow from operations/EBITDA, customer service and experience, and market share</p>
+                  <div class="acc_text">
+                    <p>Significantly <strong>improved business performance</strong> measured in terms of significant, tangible, economic benefits, including increased operating/net profit and margins, cash flow from operations/EBITDA, customer service and experience, and market share</p>
+                  </div>
                 </li>
+
                 <li id="customer_benefits2" class="acc_class"><a href="#" class="acc_link">Strategic Competitive Advantage</a>
                   <p class="acc_text"><strong>Strategic competitive advantage</strong> gained from predictive insight into the future of customer and competitor behavior, product line profitability/ROI, and trending and patterns in performance</p>
                 </li>
+
                 <li id="customer_benefits3" class="acc_class"><a href="#" class="acc_link">Increased Efficiency of Operations + Spending</a>
-                  <div class="acc_text">
-                    <p>Increased efficiency of operations and budgetary spending</p>
+                  <div class="acc_text"><strong>Increased efficiency of operations and budgetary spending</strong>
                     <ul>
                       <li type="circle">Optimally allocating scarce, valuable resources, like human and financial capital, where they will deliver the greatest business benefit and ROI</li>
                     </ul>
                   </div>
                 </li>
+
                 <li id="customer_benefits4" class="acc_class"><a href="#" class="acc_link">Pay as You Go Model</a>
-                  <div class="acc_text">
-                    <p><strong><em>Pay-as-you-go</em> payment model</strong> for analytics software &amp; solutions delivered as a service</p>
+                  <div class="acc_text"><strong><em>Pay-as-you-go</em> payment model</strong> <strong>for analytics software &amp; solutions</strong> delivered as a service
                     <ul>
                       <li type="circle">Amortizes, shared infrastructure costs for accessing a world-class analytics, business intelligence, and data warehousing infrastructure and solution</li>
                       <li type="circle">Annuitizes technology expenditures on a usage basis
@@ -98,49 +134,64 @@
                     </ul>
                   </div>
                 </li>
+
                 <li id="customer_benefits5" class="acc_class"><a href="#" class="acc_link">Reduced Implementation Risk in Rolling Out</a>
-                  <div class="acc_text"> Dramatically<strong> reduced implementation risk in rolling out</strong> analytics, business intelligence, and data warehousing solutions relative to “rolling your own”
+                  <div class="acc_text">
+                    <p>Dramatically <strong>reduced implementation risk in rolling out</strong> analytics, business intelligence, and data warehousing solutions relative to “rolling your own”<p>
                     <ul>
                       <li type="circle">Reduces technical and operational complexity underlying Analytics implementation</li>
                       <li type="circle">Analytics technology stack is operated, managed, and supported for you</li>
                     </ul>
                   </div>
                 </li>
+
                 <li id="customer_benefits6" class="acc_class"><a href="#" class="acc_link">Able to Focus on Your Core Business</a>
                   <div class="acc_text">
                     <p>Enables your firm to stay <strong>focused on your core business</strong>, while leveraging world-class Analytics solutions and resources to solve complex and complicated business problems</p>
                   </div>
                 </li>
+
               </ul>
+
             </div>
           </div>
-          <?php include '../partials/_services_right.php'; ?>
+          <!--Start of Right Column-->
+          <div class="right_col">
+            <div class="customer_benefits">
+              <h3><a class="loc" href="#customer_benefits">Customer Benefits</a></h3>
+              <ul>
+                <li>Increased business performance <a href="/services#customer_benefits1">More</a></li>
+                <li>Strategic competitive advantage <a href="/services#customer_benefits2">More</a></li>
+                <li>Increased efficiency of operations + spending <a href="/services#customer_benefits3">More</a></li>
+                <li>Pay as you go model <a href="/services#customer_benefits4">More</a></li>
+                <li>Reduced implementation risk in rolling out <a href="/services#customer_benefits5">More</a></li>
+                <li>Able to focus on your core business <a href="/services#customer_benefits6">More</a></li>
+              </ul>
+            </div>
+            <div class="action">
+              <h4>Is the Cobalt Talon solution right for you?</h4>
+              <a class="fancy" href="/contact#contact">Check It Out</a></div>
+            </div>
+          </div>
         </div>
+        <div id="root_footer"></div>
       </div>
-      <div id="root_footer"></div>
     </div>
-  </div>
-  <?php include '../partials/_footer.php'; ?>
-  <?php include '../partials/_scripts.php'; ?>
-<script type="text/javascript">
-//<![CDATA[
-$(document).ready(function(){
-  var hash = window.location.hash
-  cust_benefits(hash)
-  $('.customer_benefits a').click(function() {
-    cust_benefits(this.hash)
-    return false
-  })
-  function cust_benefits(hash) {
-    var needle=/\#customer_benefits(\d+)/
-    var match = hash.match(needle)
-    if(match) {
-      $.scrollTo('#customer_benefits', 1000);
-      $(hash + ' .acc_text').delay(1200).slideToggle()
-    }
-  }
-});
-//]]>
-</script>
+    <?php include '../partials/_footer.php'; ?>
+    <script>window.jQuery || document.write("<script src='/js/libs/jquery-1.5.1.min.js'>\x3C/script>")</script>
+<!-- scripts concatenated and minified via ant build script-->
+<script src="js/plugins.js"></script>
+<script src="js/script.js"></script>
+<!-- end scripts-->
+<!--[if lt IE 7 ]>
+    <script src="js/libs/dd_belatedpng.js"></script>
+    <script>DD_belatedPNG.fix("img, .png_bg");</script>
+  <![endif]-->
+<script>
+    var _gaq=[["_setAccount","UA-XXXXX-X"],["_trackPageview"]]; // Change UA-XXXXX-X to be your site's ID
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
+    g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
+    s.parentNode.insertBefore(g,s)}(document,"script"));
+  </script>
 </body>
 </html>
--- partials/_products_menu.php
+++ partials/_products_menu.php
@@ -1,10 +1,10 @@
 <div class="menu local">
-  <h3><a href="/products">Products</a></h3>
+  <h3><a class="norm" href="/products">Products</a></h3>
   <ul>
     <li><a href="/products#overview">Overview</a></li>
     <li><a href="/products#analytic-platform">Cobalt Talon Analytic Platform</a></li>
-    <li class="indent"><a href="/products/how-it-works">How it Works</a></li>
+    <li class="indent"><a class="norm" href="/products/how-it-works">How it Works</a></li>
     <li><a href="/products#advanced-analytic-modules">Advanced Analytic Modules</a></li>
-    <li class="indent"><a href="/products/market-shift-module">Market Shift Module&#8482;</a></li>
+    <li class="indent"><a class="norm" href="/products/market-shift-module">Market Shift Module&#8482;</a></li>
   </ul>
 </div>
