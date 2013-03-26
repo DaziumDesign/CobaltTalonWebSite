01a6a083a8e0126e79becea3eadfb1aa47945f6e
Craig Hess <craig@dazium.com>
2013-03-26 17:47:23 -0500

    New team bio member pics for all. New team member Michelle Lang added in last position.

--- pages/company.php
+++ pages/company.php
@@ -20,7 +20,7 @@
             <p>The Cobalt Talon team has combined experience and expertise in analytics, business intelligence, and data warehousing, as well as cloud computing and delivering software as a service. Our experiences enable Cobalt Talon Analytics to provide high economic-impact solutions on an affordable budget, in an extremely reasonable timeframe, addressing some of the most complex challenges that health care businesses face today.</p>
             <div class="bio-container">
               <h2>Darren Taylor</h2>
-              <img width="100" height="100" src="/images/team/darren-taylor.jpg" alt="Darren Taylor" />
+              <img src="/images/team/darren-taylor_1.jpg" width="100" height="150" alt="Darren Taylor" />
               <div class="bio-content">
                 <p>Darren Taylor serves as the President and Chief Operating Officer of Cobalt Talon. Previous to joining the Cobalt Talon team, Darren spent over 20 years with Blue Cross Blue Shield Kansas City, a mid-size health insurer serving approximately one million members. During his tenure with Blue KC, Darren served as director of a medical economics unit; oversaw a large portion of the company's Year 2000 systems consolidation project; overhauled the corporate data warehouse; and ran the company's analytics program, which he now manages with Cobalt Talon.</p>
                 <p>One of Taylor's chief professional accomplishments was leading the talented team that transformed Blue KC's data warehouse from a cost center to a profit center. His team rebuilt the corporate data warehouse and created an analytical infrastructure that runs a consolidated set of analytical models and applications. Today, under Darren's leadership, Cobalt Talon offers data warehousing and analytical infrastructure to other healthcare companies.</p>
@@ -29,7 +29,7 @@
             </div>
             <div class="bio-container">
               <h2>Bill Chamberlain</h2>
-              <img src="/images/team/bill-chamberlain.jpg" width="100" height="150" alt="Bill Chamberlain" />
+              <img src="/images/team/bill-chamberlain_1.jpg" width="100" height="150" alt="Bill Chamberlain" />
               <div class="bio-content">
                 <p>Bill Chamberlain serves as Vice President of Business Development for Cobalt Talon. Prior to joining the Cobalt Talon team, Bill served in a similar capacity for The MEDSTAT GROUP and Ingenix serving Fortune 1000 Employers and Regional Health Plans relying upon decision support systems and analytic consulting solutions to address healthcare data and reporting challenges. Most recently, Bill directed business development efforts for Hewlett-Packard's (formerly Knightsbridge Solutions, LLC) healthcare information management and business intelligence practice. During his 20 plus years in healthcare informatics industry, Bill is credited with leading both mature and start-up healthcare technology companies to exceptional top-line performance. While working at Knightsbridge Solutions, Bill was instrumental in developing BCBC of KC's world-class EDW &amp; BI platform and marketing its assets and value to Blue's plans across the country. Bill is excited to be a part of a healthcare analytics company whose origins are payer-based and whose solutions are squarely focused on addressing the analytic challenges brought about by a major shift in the payers' business model.</p>
                 <p>Bill holds a Bachelor's degree from the University of Georgia and is considered a subject matter expert in healthcare data and predictive analytics.</p>
@@ -37,7 +37,7 @@
             </div>
             <div class="bio-container">
               <h2>Carrie Cowdin</h2>
-              <img src="/images/team/carrie-cowdin.jpg" width="100" height="100" alt="Carrie Cowdin" />
+              <img src="/images/team/carrie-cowdin_1.jpg" width="100" height="150" alt="Carrie Cowdin" />
               <div class="bio-content">
                 <p>Carrie Cowdin serves as the Vice President of Product Development for Cobalt Talon. Carrie comes to Cobalt Talon from her consulting firm, Cachet Consulting, which facilitated business analysis and competitive strategy for healthcare companies.</p>
                 <p>Before starting her own business, Carrie attained experience managing several departments at Blue Cross Blue Shield of Kansas City, sustaining roles such as Director of Marketing Systems, Director of the Business Intelligence Competency Center, and Director of the Technology Service Group. Carrie was instrumental in several initiatives while at Blue KC, including the development of award winning programs in the areas of Disease Management and Predictive Modeling. In addition, she helped develop a 360 Member View Dashboard and an Enterprise Data Warehouse for the company. She brings not only many years of experience to Cobalt Talon, but also a variety of expertise.</p>
@@ -46,8 +46,20 @@
             </div>
             <div class="bio-container">
               <h2>Jeff Eckhaus</h2>
-              <p>Jeff Eckhaus is the Vice President of Services and Infrastructure for Cobalt Talon. Jeff has spent 19 years in data management, business intelligence, and analytics. Prior to joining Cobalt Talon, Jeff was a Vice President at Praeos Technologies, a data management and business intelligence consulting firm. Jeff began his data management career in the Data Warehouse practice of PriceWaterhouseCoopers. Jeff has led numerous client engagements involving strategies, methodologies, best practices, and technical implementations of data and analytical solutions. His clients include several of the Fortune 100 companies, as well as many smaller companies.</p>
-              <p>Jeff has a Computer Science degree from The Pennsylvania State University.</p>
+              <img src="/images/team/jeff-eckhaus.jpg" width="100" height="150" alt="Jeff Eckhaus" />
+              <div class="bio-content">
+                <p>Jeff Eckhaus is the Vice President of Services and Infrastructure for Cobalt Talon. Jeff has spent 19 years in data management, business intelligence, and analytics. Prior to joining Cobalt Talon, Jeff was a Vice President at Praeos Technologies, a data management and business intelligence consulting firm. Jeff began his data management career in the Data Warehouse practice of PriceWaterhouseCoopers. Jeff has led numerous client engagements involving strategies, methodologies, best practices, and technical implementations of data and analytical solutions. His clients include several of the Fortune 100 companies, as well as many smaller companies.</p>
+                <p>Jeff has a Computer Science degree from The Pennsylvania State University.</p>
+              </div>
+            </div>
+            <div class="bio-container">
+              <h2>Michelle Lang</h2>
+              <img src="/images/team/michelle-lang.jpg" width="100" height="150" alt="Michelle Lang" />
+              <div class="bio-content">
+                <p>Michelle Lang serves as the Vice President of Analytic Method Development for Cobalt Talon. Michelle comes to Cobalt Talon from Blue Cross Blue Shield of Kansas City where she spent over 18 years with the health insurance company. </p>
+                <p>While at Blue Cross Blue Shield of Kansas City, Michelle served in several roles from both an Information Technology and Business perspective. Her latest role was Director of Analytic Solutions where she helped consolidate the analytic reporting needs of the company along with providing reporting tool support. Michelle worked on the Corporate Data Warehouse team for over seven years where she was instrumental in the rebuilding of the warehouse. She also worked in the Population Health Department and HealthCare Information Services Department, where she worked closely with many of the business areas to understand and provide for their analytical reporting needs. In addition, Michelle worked in Medical Management and has experience with Utilization Review along with Case Management and Disease Management activities. Michelle also worked for a Pharmaceutical Research Company ensuring drug development protocols were followed and validated. Michelle has a vast knowledge and expertise of healthcare data from her multiple roles in this industry along with her education.  </p>
+                <p>Michelle received her Bachelor Degrees in Business Administration and Health Information Management from the University of Kansas. In addition, Michelle has a Master’s Degree in Health Care Service Administration with a concentration in Health Care Outcomes from the University of Kansas. She is credentialed as a Registered Health Information Administrator.  </p>
+              </div>
             </div>
           </div>
           <?php include '../partials/_company_right.php'; ?>
