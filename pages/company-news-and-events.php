9a0d0550037c52468b07b1332e4bc1ea297c79ad
Craig Hess <craig@dazium.com>
2013-04-23 12:27:58 -0500

    Reorderd the items on the news and events page and added 3 events. Styles added for .event to control margin and spacing.

--- pages/company-news-and-events.php
+++ pages/company-news-and-events.php
@@ -18,23 +18,41 @@
             <img class="main_image" src="/images/company/main.jpg" width="588" height="123">
             <h1 id="news-and-events">News and Events</h1>
             <p>&nbsp;</p>
+            <h2>Events</h2>
+            <p>Cobalt Talon will exhibit our Suite of Healthcare Solutions at the following meetings:</p>
+            <div class="event">
+              <h5>Healthcare Business Intelligence Summit</h5>
+              <p>Minneapolis, Minnesota<br>
+                September 19, 2013<br>
+                <a href="http://www.healthcarebisummit.com/" title="Healthcare Business Intelligence Summit - September 20, 2012">http://www.healthcarebisummit.com/</a><br />
+                <a href="http://www.healthcarebisummit.com/"><img src="/images/news-and-events/hcbussummit.png" alt="Healthcare Business Intelligence Summit" /></a>
+              </p>
+            </div>
+            <div class="event">
+              <h5>Im Symposium</h5>
+              <p>Little Rock, Arkansas<br />
+                September 22 - 25, 2013<br />
+                <a href="http://imsymposium2013.com/" title="2013 Information Management Symposium - Arkansas Blue Cross Blue Shield">http://imsymposium2013.com/</a><br />
+                <a href="http://imsymposium2013.com/"><img src="/images/news-and-events/imanagement.jpg" alt="Information Management Symposium" /></a>
+              </p>
+            </div>
+            <div class="event">
+              <h5>Western Conference</h5>
+              <p>Englewood, Colorado<br />
+                July 7 - 10, 2013<br />
+                <a href="http://westernconference.org/operations/" title="The Western Conference of Prepaid Medical Service Plans | External Operations Conference">http://westernconference.org/operations/</a><br />
+                <a href="http://westernconference.org/operations/"><img src="/images/news-and-events/westconf.jpg" alt="Western Operations Conference" /></a>
+              </p>
+            </div>
+            <h2>White Papers</h2>
+            <p>Coming Soon</p>
+            <p>&nbsp;</p>
             <h2>Press Releases</h2>
             <p>Coming Soon</p>
             <p>&nbsp;</p>
             <h2>Publications</h2>
             <p>Coming Soon</p>
             <p>&nbsp;</p>
-            <h2>White Papers</h2>
-            <p>Coming Soon</p>
-            <p>&nbsp;</p>
-            <h2>Events</h2>
-            <p>Cobalt Talon will exhibit our Suite of Healthcare Solutions at the following meetings:</p>
-            <div class="event">
-              <h5>Healthcare Business Intelligence Summit</h5>
-              <p>Minneapolis, MN<br>
-              <em>September 20, 2012</em><br>
-              <a href="http://www.healthcarebisummit.com/">www.healthcarebisummit.com</a></p>
-            </div>
           </div>
           <?php include '../partials/_company_right.php'; ?>
         </div>
